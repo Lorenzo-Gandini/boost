59c9634dcba11f9bd1254811021f5059b41d25f3
Lorenzo Gandini <lorenzo.gandini@hotmail.it>
2024-12-06 16:48:52 +0100

    Show graph of colums, max, min and avg

--- trend_analysis.py
+++ trend_analysis.py
@@ -29,3 +29,62 @@ for col in data.columns:
 if not(check):
     raise KeyError("La colonna 'Tempo (sec)' non è presente nel DataFrame. Verifica il file CSV e assicurati che i nomi delle colonne siano corretti.")
 
+# Time as x axes
+data.set_index("Tempo (sec)", inplace=True)
+
+# beginning of training phases
+fine_riscaldamento = 300    # first 5 minutes
+inizio_defaticamento = data.index.max() - 300 # last 5 minutes
+inizio_allenamento = fine_riscaldamento + 1
+
+# Define training phases
+data['Fase'] = 'Fase Allenamento'
+data.loc[data.index <= fine_riscaldamento, 'Fase'] = 'Riscaldamento'
+data.loc[data.index > fine_riscaldamento, 'Fase'] = 'Allenamento'
+data.loc[data.index >= inizio_defaticamento, 'Fase'] = 'Defaticamento'
+
+to_ignore = ['Distanza (m)','Cadenza (rpm)','Pendenza (%)']
+data = data.drop(columns = to_ignore, errors='ignore')
+
+for col in data.columns[:-1]:
+    data[col] = data[col].replace(0, np.nan)  #To interpolate change 0 with NaN
+    data[col] = data[col].interpolate(method='linear')  
+
+    metriche_fasi = data.groupby('Fase')[col].agg(['mean', 'max', 'min']).reset_index()
+
+    # Draw training phases
+    plt.figure(figsize=(15, 5))
+    plt.plot(data.index, data[col], label=col, color='blue')
+    plt.fill_between(data.index, 0, data[col], where=(data['Fase'] == 'Riscaldamento'), color='green', alpha=0.1, label='Riscaldamento')
+    plt.fill_between(data.index, 0, data[col], where=(data['Fase'] == 'Allenamento'), color='red', alpha=0.1, label='Allenamento')
+    plt.fill_between(data.index, 0, data[col], where=(data['Fase'] == 'Defaticamento'), color='blue', alpha=0.1, label='Defaticamento')
+
+    allenamento_stats = metriche_fasi[metriche_fasi['Fase'] == 'Allenamento']
+    if not allenamento_stats.empty:
+        max_value = allenamento_stats['max'].values[0]
+        min_value = allenamento_stats['min'].values[0]
+
+        max_index = data[(data['Fase'] == 'Allenamento') & (data[col] == max_value)].index[0]
+        min_index = data[(data['Fase'] == 'Allenamento') & (data[col] == min_value)].index[0]
+
+        # Draw max and min on the graph
+        plt.scatter(max_index, max_value, color='red', s=75)
+        plt.scatter(min_index, min_value, color='darkgreen', s=75)
+        
+        plt.annotate(text=f'Max : {int(max_value)}', xy=(max_index, max_value), xytext=(10, 10), textcoords='offset points', fontsize=10, color='black')
+        plt.annotate(text=f'Min : {int(min_value)}', xy=(min_index, min_value), xytext=(10, -10), textcoords='offset points', fontsize=10, color='black')
+
+    # Ad mean to the legend
+    allenamento_mean = allenamento_stats['mean'].values[0] if not allenamento_stats.empty else None
+    if allenamento_mean is not None:
+        plt.plot([], [], ' ', label=f'Media Allenamento: {allenamento_mean:.2f}')
+
+    # Draw graph
+    plt.xlabel('Tempo (sec)')
+    plt.ylabel(col)
+    plt.ylim(0, data[col].max())
+    plt.yticks(np.arange(0, (data[col].max() * 1.1) + (max(1, round((data[col].max() * 1.1) / 10))), max(1, round((data[col].max() * 1.1) / 10))))  
+    plt.title('Andamento della ' + col.split(" ")[0] + ' e Fasi di Allenamento')
+    plt.legend()
+    plt.grid()
+    plt.show()

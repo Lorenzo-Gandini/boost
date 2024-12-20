6d00b95a0e4811440d9a1d2fa15d390405707915
Lorenzo Gandini <lorenzo.gandini@hotmail.it>
2024-12-20 19:44:49 +0100

    clean the main putting some info in config

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import numpy as np
 import optitrack.csv_reader as csv
 from optitrack.geometry import *
 from utils import *
+from config import ATHLETE_MOD, MOCAP_RECORD
+
+filename = f"lab_records/{ATHLETE_MOD}_{MOCAP_RECORD}.csv"
+print(filename)
 
 # Load csv
-filename = "lab_records/Lorenzo_2.csv"
 take = csv.Take().readCSV(filename)
 
 # Get the position of all joints and give them a color
 body_edges, bones_pos, colors = get_bones_position(take)
+
 # angles = calculate_angles(bones_pos)
 # save_angles(angles)
-show_points(take, bones_pos, body_edges, colors)
 # save_spine_values(bones_pos)
 
+# show_animation(take, bones_pos, body_edges, colors)
+show_animation(take, bones_pos, body_edges, colors, [1, 14, 15])

4f128e5038bdc8c9f024d94a1146dee2ebae145f
Lorenzo Gandini <lorenzo.gandini@hotmail.it>
2025-02-27 14:41:51 +0100

    removed changes to athlete names

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from utils import ask_athlete, ask_option, ask_yesno, user_message, print_recap
 def main():
     # Interaction with the user about athlets and desired output
     athlete = ask_athlete("Which athlete do you want to analyze?")
-    athlete_mod = athlete.replace(" ", "_")
-    athlete_mod_uc = athlete_mod.upper()
-
     option = ask_option("What type of analysis do you want to run?")
     # want_pdf = ask_yesno("Do you want a PDF report?") #WORKINPROGRESS
     show_plots = ask_yesno("Do you want to see the plots during the analysis?")
@@ -34,22 +31,22 @@ def main():
     if SPINE:
         user_message("SPINE analysis will be performed.", "info")
         import spine_analysis
-        spine_analysis.run_spine_analysis(athlete, athlete_mod, athlete_mod_uc, show_plots)
+        spine_analysis.run_spine_analysis(athlete, show_plots)
 
     if KNEE:
         user_message("KNEE analysis will be performed.", "info")
         import knee_analysis
-        knee_analysis.run_knee_analysis(athlete, athlete_mod, athlete_mod_uc, show_plots)
+        knee_analysis.run_knee_analysis(athlete, show_plots)
 
     if ANKLE:
         user_message("ANKLE analysis will be performed.", "info")
         import ankle_analysis
-        ankle_analysis.run_ankle_analysis(athlete, athlete_mod, athlete_mod_uc, show_plots)
+        ankle_analysis.run_ankle_analysis(athlete, show_plots)
 
     if TRAINING:
         user_message("TRAINING analysis will be performed.", "info")
         import training_analysis
-        training_analysis.run_training_analysis(athlete, athlete_mod_uc, show_plots)
+        training_analysis.run_training_analysis(athlete, show_plots)
 
     # if PDF:
     #     user_message("A PDF report will be generated.", "info")
